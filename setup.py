8108a238ed24cce0a0ea2b106499e4b0b2533455
Gil Shotan <gilsho@cs.stanford.edu>
2014-11-16 23:02:59 -0800

    add pypi setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='kryptonite',
+    packages=['kryptonite'],
+    license='MIT',
+    version='0.1.1',
+    description='Cryptography for humans',
+    long_description='',
+    author='Gil Shotan',
+    author_email='gil@gilshotan.com',
+    url='https://github.com/gilsho/kryptonite',
+    download_url='https://github.com/gilsho/kryptonite/tarball/0.1.1',
+    keywords='cryptography security encryption decryption',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=['pycrypto', 'passlib']
+)
